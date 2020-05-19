569580cb428d80095e888dd2cf795a413c0173b6
berkan sarac <berkan.sarac@mrevyap.k12.tr>
2020-05-19 16:27:51 +0300

    .

--- jsodev/DOM3/odev2/app.js
+++ jsodev/DOM3/odev2/app.js
@@ -1,18 +1,14 @@
-let sinirlama= document.querySelector("#mesaj");   
-let yazi=document.querySelector("#yazi");   
-console.log(sinirlama)
-sinirlama.addEventListener("keyup",KarakterSayisi);
-function KarakterSayisi()
-{
-    
-    var sonsayi = 100 - document.getElementById("mesaj").value.length;
-        if (sonsayi >= 0)
-        {
-            document.getElementById("sayac").innerHTML = sonsayi;
-        }
-        else
-        {
-            document.getElementById("mesaj").value = document.getElementById("mesaj").value.substring(0, 100);
-            document.getElementById("sayac").innerHTML = 0;
-        }
+let radio1=document.getElementById("exampleRadios1");
+let radio2=document.getElementById("exampleRadios2");
+let radio3=document.getElementById("exampleRadios3");
+let radio4=document.getElementById("exampleRadios4");
+let radio5=document.getElementById("exampleRadios5");
+let radio6=document.getElementById("exampleRadios6");
+let radio7=document.getElementById("exampleRadios7");
+let radio8=document.getElementById("exampleRadios8");
+let radio9=document.getElementById("exampleRadios9");
+let progess=document.getElementById("tur");
+if(radio1.checked==true ||radio2.checked==true||radio3.checked==true){
+    progess.style.width="25%";
+
 }
