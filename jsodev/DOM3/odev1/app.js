ea9747b3002584eee592d52205d8d6803df14f3e
berkan sarac <berkan.sarac@mrevyap.k12.tr>
2020-05-22 17:51:09 +0300

    .

--- jsodev/DOM3/odev1/app.js
+++ jsodev/DOM3/odev1/app.js
@@ -1,16 +1,19 @@
 let buton=document.querySelector("#button");
 let buton2=document.querySelector("#button2");
+let buton3=document.querySelector("#button3");
 let ekran1=document.querySelector("#ekran1");
 let ekran2=document.querySelector("#ekran2");
 let bir=document.querySelector("#bir");
 let iki=document.querySelector("#iki");
 buton.addEventListener("click",ileri);
 buton2.addEventListener("click",geri);
+buton3.addEventListener("click",kaydet);
 function ileri(){
     bir.className="bg-white";
     iki.className="bg-danger";
-buton.style.display="none"
-buton2.style.display="block"
+buton.style.display="none";
+buton2.style.display="block";
+buton3.style.display="block";
 ekran1.style.display="none";
 ekran2.style.display="block";
 }
@@ -18,9 +21,13 @@ function geri(){
     iki.className="bg-white";
     bir.className="bg-danger";
     buton2.style.display="none"
+    buton3.style.display="none";
     buton.style.display="block"
     ekran1.style.display="block";
     ekran2.style.display="none";
+}
+function kaydet(){
+    alert("Kayıt edildi")
 }
     
 
